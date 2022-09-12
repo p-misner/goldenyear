54c921eb9ec8ef54d63b5428a58dfc6850352c14
p-misner <priyamisner@gmail.com>
2022-09-12 08:37:43 -0400

    server change

--- src/components/DogPage.tsx
+++ src/components/DogPage.tsx
@@ -115,7 +115,7 @@ function DogPage(props: DogProps) {
   useEffect(() => {
     async function getRecords() {
       const response = await fetch(
-        `http://localhost:3001/dogRecords/${dogName}`
+        `https://goldenyearsapp.herokuapp.com/dogRecords/${dogName}`
       );
 
       if (!response.ok) {
