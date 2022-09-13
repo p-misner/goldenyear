2090bfaf4c1c74b7b82a760461bb282b7b6cfed5
p-misner <priyamisner@gmail.com>
2022-09-13 14:17:18 -0400

    typo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dbo = require('./backend/db/conn');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build/')));
-  app.length('', (req, res) => {
+  app.get('', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 } else {
